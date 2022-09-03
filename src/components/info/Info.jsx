a65fda236bf0e03eb16668397eaf4a0480d8df24
dahyun612@naver.com <dahyun612@naver.com>
2022-09-03 16:24:48 +0900

    관리자,마이페이지styled  update

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector, useDispatch} from "react-redux/";
 import { useEffect } from "react";
+import styled from "styled-components";
 
 import { __getMember } from "../../redux/modules/member";
 
@@ -26,23 +27,62 @@ const Info = () => {
     })
     return (
         <div>
+            <InfoBox>
             <div>
-                <p>ID : {info?.id}</p>
+                <Label>
+                <p>ID : {info?.id}</p></Label>
             </div>
             <div>
-                <p>닉네임 : {info?.nickName}</p>
+            <Label>
+                <p>닉네임 : {info?.nickName}</p></Label>
             </div>
             <div>
-                <p>보유코인 : (받을 예정)</p>
+            <Label>
+                <p>보유코인 : (받을 예정)</p></Label>
             </div>
             <div>
-                <p>승리 수 : (받을 예정)</p>
+            <Label>
+                <p>승리 수 : (받을 예정)</p></Label>
             </div>
             <div>
-                <button onClick={()=>{navigate("/login")}}>회원탈퇴</button>
+                <Button onClick={()=>{navigate("/login")}}>회원탈퇴</Button>
             </div>
+            </InfoBox>
         </div>
     )
 }
 
 export default Info;
+
+const InfoBox = styled.div `
+width:450px;
+margin: 0 auto;
+margin-top: 4rem;
+border : 4px solid #eee;
+border-radius: 12px;
+padding:12px 24px 24px 24px;
+background-size: 240px;
+
+`;
+const Label = styled.label `
+    overflow: hidden;
+    display: block;
+    width: 100%;
+    margin: 0 0 8px 0;
+    border: 1px solid #eee;
+    color: #8F8F91;
+`;
+
+const Button = styled.button `
+    border: 1px solid #333;
+    background: #333;
+    color: #fff;
+    font-size: 17px;
+    padding: 13px 0;
+    margin: 15px 0 0;
+    width: 400px;
+    display: inline-block;
+    text-align: center;
+    font-weight: 400;
+    text-transform: uppercase;
+`;
