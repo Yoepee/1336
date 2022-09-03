98f11a9696b65db1e20b1f235842f2af7e28fb9d
dahyun612@naver.com <dahyun612@naver.com>
2022-09-03 21:10:16 +0900

    styled 추가

--- src/components/choice/Choice.jsx
+++ src/components/choice/Choice.jsx
@@ -10,7 +10,6 @@ const Choice = () => {
                 <StButton onClick={() => { navigate("/chart") }}> 랭킹보기 </StButton>
                 <StButton onClick={() => { navigate("/admin/:id") }}> 관리자 </StButton>
             <div>
-<<<<<<< HEAD
                 
             </div>
             <div>
@@ -20,19 +19,6 @@ const Choice = () => {
                 <Button onClick={() => { navigate("/game/dice") }}> 주사위 </Button>
                 <Button onClick={() => { navigate("/game/lotto") }}> 로또 </Button>
             </GameBox>
-=======
-                {/* 메인화면에서 랭킹과 관리자로 연결되는 부분(아직 미정) */}
-                <p>전체게임</p>
-                <button onClick={() => { navigate("/chart") }}> 랭킹보기 </button>
-                <button onClick={() => { navigate("/admin/:id") }}> 관리자 </button>
-            </div>
-            <div>
-                {/* 버튼클릭시 게임페이지 경로로 이동 */}
-                <button onClick={() => { navigate("/game/counter") }}> 카운터 </button>
-                <button onClick={() => { navigate("/game/oddeven") }}> 홀짝 </button>
-                <button onClick={() => { navigate("/game/dice") }}> 주사위 </button>
-                <button onClick={() => { navigate("/game/lotto") }}> 로또 </button>
->>>>>>> 00f2327af4deb80421cebaab8403c94108c992b4
             </div>
         </div>
     )
@@ -83,7 +69,7 @@ const StButton = styled.button `
     white-space: nowrap;
     max-width: 100%;
     height: 42px;
-    font-size: 20px;
+    font-size: 15px;
     padding: 0px 22px;
     min-width: 64px;
     background-color: transparent;
